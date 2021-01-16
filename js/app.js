7d8f9fd7c9457b857e7b549917b4050289d5a800
assurdafragola <assurdafragola@gmail.com>
2021-01-16 22:56:33 +0100

    complete/delete/important

--- js/app.js
+++ js/app.js
@@ -6,11 +6,29 @@ document.addEventListener("DOMContentLoaded", () => {
     const addButton = document.querySelector(".add_button")
     const closeAddButton = document.querySelector(".add_popup_content_btn_close")
     
-    const toggleAddPopup = () => {
-        addPopup.classList.toggle("active")
+    const toggleAddPopup = (element) => {
+        element.classList.toggle("active")
     }
-    addButton.addEventListener("click", toggleAddPopup)
-    closeAddButton.addEventListener("click", toggleAddPopup)
+
+    addButton.addEventListener("click", function () {toggleAddPopup(addPopup)})
+    closeAddButton.addEventListener("click", function () {toggleAddPopup(addPopup)})
+
+    const deletePopup = document.querySelector(".delete_popup")
+    const closeDeleteButton = document.querySelector(".delete_popup_content_btn_close")
+
+    const confirmDeleteBtn = document.querySelector(".delete_popup_content_btn_submit")
+
+    closeDeleteButton.addEventListener("click", function () {toggleAddPopup(deletePopup)})
+    confirmDeleteBtn.addEventListener("click", function () {toggleAddPopup(deletePopup)})
+
+    const deleteTaskPermanently = (e) => {
+        console.log(e.currentTarget.parentElement)
+        e.currentTarget.parentElement.remove()
+    }
+
+
+
+    // confirmDeleteBtn.addEventListener("click", )
 
     const saveAddButton = document.querySelector(".add_popup_content_btn_submit")
     
@@ -29,6 +47,30 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log(currentDate)
 
     let tasks = []
+    let completedTasks = []
+
+    const completeTask = (e) => {
+        console.log("checked")
+
+        e.currentTarget.classList.toggle("inactive")
+        e.currentTarget.parentElement.classList.toggle("completed")
+    }
+
+    const importantTask = (e) => {
+        console.log("important")
+        
+        e.currentTarget.classList.toggle("important")
+    }
+
+    const editTask = () => {
+        console.log("edit")
+    }
+
+    const deleteTask = () => {
+        console.log("delete")
+        toggleAddPopup(deletePopup)
+    }
+    
 
     const saveNewTask = () => {
 
@@ -51,16 +93,35 @@ document.addEventListener("DOMContentLoaded", () => {
         newTask.id = `taskNo${tasks.length}`
         newTask.classList.remove("hidden")
 
+        newTask.children[0].addEventListener("click", completeTask)
+        
+        newTask.children[3].addEventListener("click", importantTask)
+
+        newTask.children[4].addEventListener("click", editTask)
+
+        newTask.children[5].addEventListener("click", deleteTaskPermanently)
+        
         task.after(newTask);
         
         console.log(newTask)
 
-        toggleAddPopup();
+        toggleAddPopup(addPopup);
         newTaskInput.value = "";
         newTaskDDL.value = "";
 
     }
     saveAddButton.addEventListener("click", saveNewTask)
+
+    const checkTaskBtn = document.querySelectorAll(".list_task_checked")
+    
+    
+
+
+  
+
+    
+
+    checkTaskBtn[0].addEventListener("click", completeTask)
 
 })
 
